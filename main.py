51493a80bab065454a9069d81f2949ecb09d1783
Dawid H <32899009+DawidPL@users.noreply.github.com>
2020-09-02 13:29:28 +0200

    refactoring

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from scraper import get_phrase
 
 
 def handle_click() -> None:
-    input_phares = (input_text.get('1.0', tk.END))
-    replaced_input_phares = input_phares.replace('\n', ' ')
-    wanted_tuple = tuple(replaced_input_phares.split())
+    input_phrases = (input_text.get('1.0', tk.END))
+    replaced_input_phrase = input_phrases.replace('\n', ' ')
+    wanted_tuple = tuple(replaced_input_phrases.split())
     get_phrase(*wanted_tuple)
 
 
@@ -16,16 +16,16 @@ def clear_input() -> None:
 
 
 window = tk.Tk()
-window.title("Internet PLUS SEO Tool")
+window.title("SEO Phrases Tool")
 entry = tk.Entry()
-button = tk.Button(master=window, text="Sprawdź frazy", command=handle_click, height=2, width=20)
+check_button = tk.Button(master=window, text="Sprawdź frazy", command=handle_click, height=2, width=20)
 delete_button = tk.Button(master=window, text="Wyczyść frazy", command=clear_input, height=2, width=20)
 input_text = tk.Text()
 input_text.pack()
-w = tk.Label(master=window, text="Wpisz szukane frazy jedna pod drugą a następnie kliknij poniżej. \n"
+main_label = tk.Label(master=window, text="Wpisz szukane frazy jedna pod drugą a następnie kliknij poniżej. \n"
                                  "Szukanie chwilę potrwa a gdy przycisk będzie ponownie aktywny wygenerowany plik .txt znajdziesz w folderze (nazwa to 'fraza + data').\n"
                                  "Teraz możesz zamknąć program lub wyczyść okno i sprawdź kolejne frazy")
-w.pack()
-button.pack()
+main_label.pack()
+check_button.pack()
 delete_button.pack()
 window.mainloop()
